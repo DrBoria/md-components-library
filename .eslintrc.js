42e9c6bca5209ca56d0479bab8b86795077bee8a
MikitaDusmikeev <dr.boria@gmail.com>
2021-11-22 13:58:13 +0300

    feature: added eslint import plugin in eslint, updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'airbnb-typescript',
+    'plugin:import/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:eslint-comments/recommended',
     'plugin:promise/recommended',
